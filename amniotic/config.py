36d413af817e61bf85364e411efddfa3047b4a9d
Edward Brown <ed@frontmatter.ai>
2022-05-28 14:24:05 +0100

    Allow JSON config file

--- amniotic/config.py
+++ amniotic/config.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from _socket import gethostname
 from dataclasses import dataclass
@@ -40,13 +41,13 @@ class Config:
     @classmethod
     def from_file(cls):
 
-        path_config_base = getenv('SC_CONFIG_BASE')
-        if not path_config_base:
-            path_config_base = APP_DIRS.user_config_dir
+        path_config = getenv('AMNIOTIC_CONFIG_PATH')
 
-        path_config_base = Path(path_config_base).absolute()
-        path_config_base.mkdir(parents=True, exist_ok=True)
-        path_config = path_config_base / 'config.yml'
+        if not path_config:
+            path_config = Path(APP_DIRS.user_config_dir) / 'config.yml'
+            path_config.parent.mkdir(parents=True, exist_ok=True)
+
+        path_config = Path(path_config).absolute()
 
         if not path_config.exists():
             msg = f'Config file not found at "{path_config}". Default values will be used.'
@@ -55,7 +56,15 @@ class Config:
         else:
             msg = f'Config file found at "{path_config}"'
             logging.info(msg)
-            config = yaml.safe_load(Path(path_config).read_text())
+
+            config_str = Path(path_config).read_text()
+            if path_config.suffix in {'.yml', '.yaml'}:
+                config = yaml.safe_load(config_str)
+            elif path_config.suffix in {'.json'}:
+                config = json.loads(config_str)
+            else:
+                msg = f'Unknown config format "{path_config.suffix}"'
+                raise ValueError(msg)
 
         config = cls(**config)
         return config
