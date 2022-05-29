81bd27d49cb3104ec0ceef23f5892b42e32e91b3
Edward Brown <ed@frontmatter.ai>
2022-05-29 16:18:14 +0100

    Add Home Assistant addon

---
+++ ha/addon/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bashio
+
+export AMNIOTIC_CONFIG_PATH="/data/options.json"
+export AMNIOTIC_MQTT_HOST="$(bashio::services mqtt 'host')"
+export AMNIOTIC_MQTT_PASSWORD="$(bashio::services mqtt 'password')"
+export AMNIOTIC_MQTT_PORT="$(bashio::services mqtt 'port')"
+export AMNIOTIC_MQTT_USERNAME="$(bashio::services mqtt 'username')"
+export AMNIOTIC_PATH_AUDIO="/media/$(bashio::config 'target')"
+
+amniotic
