e7e1fedb4224b8440bc71b18e40e58d8eeb18eed
Edward Brown <ed@frontmatter.ai>
2022-05-29 16:22:16 +0100

    Add audio_subdirectory option

--- ha/addon/run.sh
+++ ha/addon/run.sh
@@ -5,6 +5,6 @@ export AMNIOTIC_MQTT_HOST="$(bashio::services mqtt 'host')"
 export AMNIOTIC_MQTT_PASSWORD="$(bashio::services mqtt 'password')"
 export AMNIOTIC_MQTT_PORT="$(bashio::services mqtt 'port')"
 export AMNIOTIC_MQTT_USERNAME="$(bashio::services mqtt 'username')"
-export AMNIOTIC_PATH_AUDIO="/media/$(bashio::config 'target')"
+export AMNIOTIC_PATH_AUDIO="/media/$(bashio::config 'audio_subdirectory')"
 
 amniotic
