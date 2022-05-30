faf30160b40874a6768695879101e28ef32fa231
Edward Brown <ed@frontmatter.ai>
2022-05-30 10:19:10 +0100

    Refine Addon config

--- ha/addon/run.sh
+++ ha/addon/run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/with-contenv bashio
 
 export AMNIOTIC_IS_ADDON=true
+export AMNIOTIC_CONFIG_PATH="/data/options.json"
 export AMNIOTIC_MQTT_HOST="$(bashio::services mqtt 'host')"
 export AMNIOTIC_MQTT_PASSWORD="$(bashio::services mqtt 'password')"
 export AMNIOTIC_MQTT_PORT="$(bashio::services mqtt 'port')"
 export AMNIOTIC_MQTT_USERNAME="$(bashio::services mqtt 'username')"
 export AMNIOTIC_PATH_AUDIO="/media/$(bashio::config 'audio_subdirectory')"
-export AMNIOTIC_LOCATION="$(bashio::config 'location')"
 
 amniotic
