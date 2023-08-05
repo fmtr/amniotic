32432ecf02bb086af0f0e912a6f9d3fa82a201a5
Edward Brown <ed@frontmatter.ai>
2023-08-05 13:29:00 +0100

    Fix duplicate device descriptions not being disambiguated

--- amniotic/audio.py
+++ amniotic/audio.py
@@ -48,8 +48,8 @@ def get_devices_raw() -> vlc.AudioOutputDevice:
     logging.info(f'Loaded temporary player to get devices...')
     player = load_new_player()
     devices_raw = player.audio_output_device_enum()
-    unload_player(player)
     logging.info(f'Unloading temporary player.')
+    unload_player(player)
     return devices_raw
 
 def get_devices(player: Optional[vlc.MediaPlayer] = None, device_names: dict[str, str] = None) -> dict[str, str]:
@@ -59,26 +59,33 @@ def get_devices(player: Optional[vlc.MediaPlayer] = None, device_names: dict[str
 
     """
 
+    log_devices = False
     if player:
         devices_raw = player.audio_output_device_enum()
     else:
         devices_raw = get_devices_raw()
+        log_devices = True
 
-    devices = {}
-    device_names = device_names or {}
+    devices_pairs = []
     if devices_raw:
         device_raw = devices_raw
-        count = 0
         while device_raw:
-            count += 1
             device_raw = device_raw.contents
             description = device_raw.description.decode()
             device = device_raw.device.decode()
-            count = len([key for key in devices.keys() if key == description])
-            if count:
-                description = f'{description} ({count + 1})'
-            devices[device] = device_names.get(description, description)
+            devices_pairs.append((device, description))
             device_raw = device_raw.next
+
+    devices = {}
+    device_names = device_names or {}
+    for device_id, description in devices_pairs:
+        count = len([value for value in devices.values() if value == description])
+        if count:
+            description = f'{description} ({count + 1})'
+        devices[device_id] = device_names.get(description, description)
+
+    if log_devices:
+        logging.info(f'Found devices: {devices}')
 
     return devices
 
