7095903dc5e2b12786299d02757afca7e5393a81
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-11-24 12:05:36 +0000

    Implement cross-control updating and re-announcing

--- amniotic/v2/sb_mqtt.py
+++ amniotic/v2/sb_mqtt.py
@@ -1,10 +1,13 @@
 import asyncio
 import logging
+from dataclasses import dataclass
+from functools import cached_property
 
 from amniotic.paths import paths
 from amniotic.v2.api import ApiAmniotic
 from amniotic.v2.recording import RecordingDefinition
 from fmtr.tools import Constants
+from haco.button import Button
 from haco.client import ClientHaco
 from haco.constants import MQTT_HOST
 from haco.device import Device
@@ -15,14 +18,38 @@ handler = logger.LogfireLoggingHandler()
 logging.basicConfig(handlers=[handler], level=logging.DEBUG)
 
 
+@dataclass(kw_only=True)
 class SelectRecording(Select):
 
     def command(self, value):
         value
 
 
+@dataclass(kw_only=True)
+class ButtonChangeTheme(Button):
+
+    async def command(self, value):
+        self.device.select_recording.options = ['a', 'b', 'c']
+        await self.device.select_recording.announce()
+        value
+
+
+@dataclass(kw_only=True)
 class Amniotic(Device):
     DEFINITIONS = [RecordingDefinition(paths.example_700KB), RecordingDefinition(paths.gambling)]  # All those on disk.
+
+    def __post_init__(self):
+        # super().__post_init__()
+        self.controls = [self.select_recording, self.btn_change_theme]
+
+    @cached_property
+    def select_recording(self):
+        return SelectRecording(name="Recordings", options=[str(defin.path) for defin in self.DEFINITIONS])
+
+    @cached_property
+    def btn_change_theme(self):
+        return ButtonChangeTheme(name="Change Theme")
+
 
 
 class ClientAmniotic(ClientHaco):
@@ -43,8 +70,7 @@ class ClientAmniotic(ClientHaco):
 
 
 async def main():
-    sel1 = SelectRecording(name="Recordings", options=[str(defin.path) for defin in Amniotic.DEFINITIONS])
-    device = Amniotic(name=f"{Constants.DEVELOPMENT} Amniotic", controls=[sel1])
+    device = Amniotic(name=f"{Constants.DEVELOPMENT} Amniotic")
 
     client = ClientAmniotic(hostname=MQTT_HOST, device=device, logger=logging.getLogger(__name__))
     await client.start()
