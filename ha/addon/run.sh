5802ab8ce0cbe369e72753feea163b9ca3d6246c
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-12-04 16:04:03 +0000

    Fix MQTT username reference in addon

--- ha/addon/run.sh
+++ ha/addon/run.sh
@@ -5,7 +5,7 @@ export HOME_ASSISTANT_URL="http://supervisor/core/api"
 
 export AMNIOTIC__MQTT__HOSTNAME="$(bashio::services mqtt 'host')"
 export AMNIOTIC__MQTT__PORT="$(bashio::services mqtt 'port' || echo 1883)"
-export AMNIOTIC__MQTT__USERNAME="$(bashio::config 'username')"
+export AMNIOTIC__MQTT__USERNAME="$(bashio::services mqtt 'username')"
 export AMNIOTIC__MQTT__PASSWORD="$(bashio::services mqtt 'password')"
 
 
