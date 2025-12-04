aa0283c43b830f7fc6326b9c04d1809c80122464
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-12-04 15:37:13 +0000

    Fix port mapping in addon and add logger instrumentation in MQTT client

--- amniotic/client.py
+++ amniotic/client.py
@@ -2,6 +2,7 @@ import asyncio
 
 from amniotic.api import ApiAmniotic
 from amniotic.device import Amniotic
+from amniotic.obs import logger
 from haco.client import ClientHaco
 
 
@@ -15,6 +16,7 @@ class ClientAmniotic(ClientHaco):
     def __init__(self, device: Amniotic, *args, **kwargs):
         super().__init__(device=device, *args, **kwargs)
 
+    @logger.instrument('Starting MQTT client {self.username}@{self.hostname}:{self.port}...')
     async def start(self):
         await asyncio.gather(
             super().start(),
