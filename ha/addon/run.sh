2c7568a64cbdabc03d3c69cfcc8219cb7c7465c0
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-12-04 10:34:15 +0000

    Fix environment variable name in addon

--- ha/addon/run.sh
+++ ha/addon/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
 
-export AMNIOTIC__HOME_ASSISTANT_URL="http://supervisor"
+export HOME_ASSISTANT_URL="http://supervisor"
 
 export AMNIOTIC__MQTT__HOSTNAME="$(bashio::services mqtt 'host')"
 export AMNIOTIC__MQTT__PORT="$(bashio::services mqtt 'port')"
