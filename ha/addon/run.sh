9e5ff921d8af7d6ff50efe1a6decd0ee9a263b17
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-12-09 10:14:52 +0000

    Refine addon environment/options handling

--- ha/addon/run.sh
+++ ha/addon/run.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
-amniotic-convert-options-env
-source /addon.env
-
 export FMTR_DEV="$(bashio::config 'fmtr_dev')"
 
 if bashio::var.true "${FMTR_DEV}"; then
     bashio::log.info "Starting SSH Development Server"
-    export FMTR_LOG_LEVEL=DEBUG
     printenv > /addon.env
     echo "root:password" | chpasswd
     /usr/sbin/sshd -D -o Port=22 -o PermitRootLogin=yes -o PasswordAuthentication=yes -o AllowTcpForwarding=yes -o LogLevel=VERBOSE
