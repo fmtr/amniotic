21ce4ee5fff419cd49c5613ef37035ab1c59544e
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2026-02-15 11:47:22 +0000

    Use production corio and haco

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from fmtr.tools import Setup
 setup = Setup(
     dependencies=dict(
         install=[
-            'corio[version.dev,logging,sets,yaml,debug,caching,api,mqtt,path.app,tabular,av,ha.api,http,youtube]==2.0.0a3',
-            'haco==0.2.0a0',
+            'corio[version.dev,logging,sets,yaml,debug,caching,api,mqtt,path.app,tabular,av,ha.api,http,youtube]',
+            'haco',
         ]
     ),
     description='A multi-output ambient sound mixer for Home Assistant',
